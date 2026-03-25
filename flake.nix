be8b383bc5228de03ba32c092ed669afaf9921e6
peter <66807853+peterc-s@users.noreply.github.com>
2026-03-25 23:09:01 +0000

    build: Update Nix flake (#301)

--- flake.nix
+++ flake.nix
@@ -1,43 +1,84 @@
 {
   description = "An Infinitely Large Napkin";
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.05";
+    nixpkgs.url = "https://channels.nixos.org/nixpkgs-unstable/nixexprs.tar.xz";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
     with flake-utils.lib;
-    eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-        tex = (pkgs.texlive.combine {
-          inherit (pkgs.texlive)
-          scheme-medium
-          asymptote wrapfig amsmath ulem hyperref capt-of
-          latexmk biber xpatch
-          tkz-graph tikz-cd xcolor todonotes
-          mdframed mathtools braket
-          multirow prerex cleveref
-          wasysym stmaryrd
-          microtype relsize
-          answers etoolbox minitoc thmtools zref needspace
-          biblatex xypic;
-        });
-      in {
-        formatter = nixpkgs.legacyPackages."${system}".nixfmt;
-        defaultPackage = pkgs.stdenv.mkDerivation {
-          name = "napkin";
-          version = "1.5";
-          src = ./.;
-          buildInputs = with pkgs; [tex ghostscript asymptote biber];
-          buildPhase = ''
-            mkdir asy
-            latexmk -f
-          '';
-          installPhase = ''
-            mkdir -p $out
-            cp Napkin.pdf $out/
-          '';
-        };
-      });
+      eachDefaultSystem (
+        system: let
+          pkgs = import nixpkgs {inherit system;};
+          tex = pkgs.texlive.combine {
+            inherit
+              (pkgs.texlive)
+              amsmath
+              answers
+              asymptote
+              biber
+              biblatex
+              braket
+              capt-of
+              cleveref
+              etoolbox
+              hyperref
+              latexmk
+              mathtools
+              mdframed
+              microtype
+              minitoc
+              multirow
+              needspace
+              pdfcol
+              prerex
+              relsize
+              scheme-medium
+              stmaryrd
+              tcolorbox
+              thmtools
+              tikz-cd
+              tikzfill
+              tkz-graph
+              todonotes
+              ulem
+              wasysym
+              wrapfig
+              xcolor
+              xpatch
+              xypic
+              zref
+              ;
+          };
+          src = builtins.path {
+            path = ./.;
+            name = "napkin";
+          };
+        in {
+          formatter = nixpkgs.legacyPackages.${system}.alejandra;
+          defaultPackage = pkgs.stdenv.mkDerivation {
+            inherit src;
+            name = "napkin";
+            version = "1.6";
+            buildInputs = with pkgs; [
+              asymptote
+              biber
+              ghostscript
+              tex
+            ];
+            buildPhase = ''
+              mkdir -p asy
+              latexmk -f
+            '';
+            installPhase = ''
+              mkdir -p $out
+              cp Napkin.pdf $out/
+            '';
+          };
+        }
+      );
 }
